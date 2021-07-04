fd30c1646a3877f249bdbf92489267865e68edd6
Rakesh Patel <rakesh.patel@maximess.com>
2021-07-04 14:27:44 +0530

    intial commit added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let abc = "Hello";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {abc} <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
